79708f8859bc5a89a8ac631c922c6f779b6526e8
Janusz Janusz <antyphone@gmail.com>
2025-11-17 02:29:55 +0100

    Fixed kernel version, not combatible with AVX-512

--- configuration.nix
+++ configuration.nix
@@ -359,7 +359,8 @@
   };
 
   # Kernel ChacheOS
-  boot.kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V4"; };
+  boot.kernelPackages = pkgs.linuxPackages_cachyos.cachyOverride { mArch = "GENERIC_V3"; }; # https://wiki.cachyos.org/features/optimized_repos/
+# Intel 12th gen (Alder Lake) and newer CPUs may report x86-64-v4 support, but in practice they cannot run AVX-512 instructions.
   # services.scx.enable = true;
 
   programs.adb.enable = true;
