6549873e65adb80377701b31f60f91104e73653e
janusz-bit <janusz-bit@proton.me>
2025-11-23 09:51:31 +0100

    Reinstall and fixes

--- 15IRX10.nix
+++ 15IRX10.nix
@@ -16,53 +16,17 @@
     "${inputs.nixos-hardware}/common/pc/ssd"
   ];
 
-
   boot.extraModulePackages = [ config.boot.kernelPackages.lenovo-legion-module ];
 
   services.thermald.enable = lib.mkDefault true;
 
   hardware = {
-    graphics = {
-      enable = true;
-      enable32Bit = true;
-      extraPackages = with pkgs; [
-        # nvidia-vaapi-driver
-        intel-media-driver # Recommended only with Intel iGPU
-      ];
-    };
     nvidia = {
-      open = true;
-      nvidiaSettings = true;
-      powerManagement.enable = true;
-      powerManagement.finegrained = false;
-      modesetting.enable = true; # Mandatory for Wayland
-
-      package = config.boot.kernelPackages.nvidiaPackages.production;
-
       prime = {
-        offload = {
-          enable = true;
-          enableOffloadCmd = true;
-        };
-
         intelBusId = "PCI:0:2:0";
         nvidiaBusId = "PCI:1:0:0";
 
       };
-    };
-  };
-
-  specialisation = {
-    gaming-time.configuration = {
-
-      hardware.nvidia = {
-        prime.sync.enable = lib.mkForce true;
-        prime.offload = {
-          enable = lib.mkForce false;
-          enableOffloadCmd = lib.mkForce false;
-        };
-      };
-
     };
   };
 }
